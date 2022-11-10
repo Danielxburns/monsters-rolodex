9bb29f3d1a563b408cebf38cabdd10d8a708623b
Danielxburns <danielburnstech@gmail.com>
2022-11-10 15:47:53 -0600

    Updates

---
+++ precache-manifest.51820dd0a83fc59509a2247fbb460689.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "64cee8c9d5ecd1fe5a9e",
+    "url": "/monsters-rolodex/static/css/main.fcddb913.chunk.css"
+  },
+  {
+    "revision": "64cee8c9d5ecd1fe5a9e",
+    "url": "/monsters-rolodex/static/js/main.b1817d25.chunk.js"
+  },
+  {
+    "revision": "bc427f4d129483714cce",
+    "url": "/monsters-rolodex/static/js/runtime~main.4f17fa38.js"
+  },
+  {
+    "revision": "e3827628e1d5f3e6a3fe",
+    "url": "/monsters-rolodex/static/js/2.69817d3e.chunk.js"
+  },
+  {
+    "revision": "e39a649d25b135c527cd",
+    "url": "/monsters-rolodex/static/js/3.63eae4ac.chunk.js"
+  },
+  {
+    "revision": "e75d740280c07ee74e128fbba1db5215",
+    "url": "/monsters-rolodex/index.html"
+  }
+];
